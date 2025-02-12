f93e39c177990f9200be747a09c3b3362ba292dc
Ylenia <yleniamighela@gmail.com>
2025-02-12 16:58:50 +0100

    added second part main (to modify)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,17 @@ export default function Main() {
         <main>
 
             <div className="contenitore_black">
-
                 <span>qui ci sarà una parte del main</span>
             </div>
+
+            <section className="contenitore_blue">
+                <a href="#"><img src="./img/buy-comics-digital-comics.png" alt="" />Digital Comics</a>
+                <a href="#"><img src="./img/buy-comics-merchandise.png" alt="" />Digital Comics</a>
+                <a href="#"><img src="./img/buy-comics-subscriptions.png" alt="" />Digital Comics</a>
+                <a href="#"><img src="./img/buy-comics-shop-locator.png" alt="" />Digital Comics</a>
+                <a href="#"><img src="./img/buy-dc-power-visa.svg" alt="" />Digital Comics</a>
+
+            </section>
 
         </main>
     );
