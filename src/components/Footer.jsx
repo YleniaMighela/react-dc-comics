a6205bf63e162eb0db9792e896d53145ed1abe1c
Ylenia <yleniamighela@gmail.com>
2025-02-12 17:48:40 +0100

    added first part fotter (to modify)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,56 @@
 // esporto la funzione Footer
 export default function Footer() {
-    return (<footer>Qui andrà parte del footer </footer>);
+    return (
+        <footer>
+
+            <div className="footer_info">
+
+                <ul className="link_footer">
+                    <li ><a href="#"></a>DC COMICS</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+
+                    <li ><a href="#"></a>DC COMICS</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+
+                </ul>
+
+                <ul className="link_footer">
+                    <li ><a href="#"></a>DC COMICS</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+
+                </ul>
+
+                <ul className="link_footer">
+                    <li ><a href="#"></a>DC COMICS</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+                    <li><a href="#"></a> Characters</li>
+
+                </ul>
+
+
+            </div>
+
+
+            <section>
+                <div className="footer_follow">
+
+                </div>
+            </section>
+        </footer>
+    );
 }
