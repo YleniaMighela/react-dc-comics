7f7f9224a3d76077099019ed11dedab105305c4d
Ylenia <yleniamighela@gmail.com>
2025-02-13 14:46:07 +0100

    fixed first part Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,42 +5,60 @@ export default function Footer() {
 
             <div className="footer_info">
 
-                <ul className="link_footer">
-                    <li ><a href="#"></a>DC COMICS</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
+                <div className="link_footer">
 
-                    <li ><a href="#"></a>DC COMICS</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
+                    <ul>
+                        <li><h4>DC COMICS</h4></li>
+                        <li><a href="#"></a> Characters</li>
+                        <li><a href="#"></a> Comics</li>
+                        <li><a href="#"></a> Movies</li>
+                        <li><a href="#"></a> TV</li>
+                        <li><a href="#"></a> Games</li>
+                        <li><a href="#"></a> Videos</li>
+                        <li><a href="#"></a> News</li>
+                    </ul>
 
-                </ul>
 
-                <ul className="link_footer">
-                    <li ><a href="#"></a>DC COMICS</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
 
-                </ul>
+                    <ul>
+                        <li><h4>SHOP</h4></li>
+                        <li><a href="#"></a> Shop DC </li>
+                        <li><a href="#"></a> Shop DC Collectibles</li>
+                    </ul>
+                </div>
 
-                <ul className="link_footer">
-                    <li ><a href="#"></a>DC COMICS</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
-                    <li><a href="#"></a> Characters</li>
+                <div className="link_footer">
 
-                </ul>
+                    <ul >
+                        <li><h4>DC</h4></li>
+                        <li><a href="#"></a> Term Of Use</li>
+                        <li><a href="#"></a> Privacy Policy </li>
+                        <li><a href="#"></a> Ad Choices</li>
+                        <li><a href="#"></a> Advertising</li>
+                        <li><a href="#"></a> Jobs</li>
+                        <li><a href="#"></a> Subscriptions</li>
+                        <li><a href="#"></a> Talent Worshops</li>
+                        <li><a href="#"></a> CPSC Certificates</li>
+                        <li><a href="#"></a> Ratings</li>
+                        <li><a href="#"></a> Shop Help</li>
+                        <li><a href="#"></a> Contact Us</li>
+
+                    </ul>
+                </div>
+
+                <div className="link_footer">
+                    <ul >
+                        <li><h4>SITES</h4></li>
+                        <li><a href="#"></a> DC</li>
+                        <li><a href="#"></a> MAD Magazine</li>
+                        <li><a href="#"></a> DC Kids</li>
+                        <li><a href="#"></a> DC Universe</li>
+                        <li><a href="#"></a> DC Power Visa</li>
+
+                    </ul>
+                </div>
+
+                <div className="logo_footer"></div>
 
 
             </div>
