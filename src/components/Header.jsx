e990474723e82bbac913d3f0860641def06c14a7
Ylenia <yleniamighela@gmail.com>
2025-02-13 08:57:44 +0100

    fixed Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,25 @@ export default function Header() {
     return (
         <header>
 
-            <div>
-                <img src="./img/dc-logo.png" alt="" />
+            <div >
+                <img id="img_logo" src="./img/dc-logo.png" alt="" />
             </div>
 
             <nav>
                 <ul>
                     <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
                 </ul>
             </nav>
+
         </header>
     );
 }
